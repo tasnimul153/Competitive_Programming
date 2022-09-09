a6cbaa61602b956909b1581adef37272af4b3dd0
Tasnimul Alam <tohiralam153@gmail.com>
2022-09-09 17:16:14 +0600

    Reverse Array 1.0

--- Problem_Solving/Array/Reverse the array/main.cpp
+++ Problem_Solving/Array/Reverse the array/main.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#define endl '\n'
 
 using namespace std;
 
 void reverseArr(int arr[]) {
-
+    int sz = sizeof(arr[0])/sizeof(arr);
+    for (int i = 0; i < sz-1; i++) {
+        
+    }
 }
 
 int main() {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     reverseArr(arr);
-    for(int i : arr) {
+    for(int i : arr) 
         cout << i << " ";
-    }
-    cout << "\n";
+    cout << endl;
     return 0;
 }
